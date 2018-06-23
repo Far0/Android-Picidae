80c719a2f842b7fc2116299bb6efb7193683cf3a
Far <->
2018-06-23 04:27:55 +0300

    Fixed the SETX problem.

--- Android Picidae/Form1.Designer.vb
+++ Android Picidae/Form1.Designer.vb
@@ -23,22 +23,44 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(360, 188)
+        Me.Button1.Location = New System.Drawing.Point(320, 403)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(76, 29)
+        Me.Button1.Size = New System.Drawing.Size(88, 41)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Install ADB and Fastboot"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(414, 409)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(76, 29)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Donate"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(167, 114)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(76, 300)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(502, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -47,4 +69,6 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- Android Picidae/Form1.vb
+++ Android Picidae/Form1.vb
@@ -1,14 +1,48 @@
 ﻿Public Class Form1
-    Private Const Text1 As String = "Installation was successful."
+
+    ''' <summary>
+    ''' Strings used in the project.
+    ''' Made by FAR
+    ''' Email:
+    ''' </summary>
+    ''' 
+    Private Const Cmd As String = "cmd.exe /c"
+    Private Const Platform As String = "powershell.exe Invoke-WebRequest -Uri http://dl.google.com/android/repository/platform-tools-latest-windows.zip -OutFile C:\Picidae_ADB\ADB_FASTBOOT.zip && "
+    Private Const UnzipAdb As String = "powershell.exe Expand-Archive C:\Picidae_ADB\ADB_FASTBOOT.zip -DestinationPath C:\Picidae_ADB && "
+    Private Const MoveAdb As String = "powershell.exe Invoke-WebRequest -Uri https://raw.githubusercontent.com/Far0/Picidae-Modules/master/move.ps1 -OutFile C:\Picidae_ADB\Move_picidae.ps1 && powershell C:\Picidae_ADB\Move_picidae.ps1 && "
+    Private Const EnvPath As String = "setx Picidae_ADB C:\Picidae_ADB\ && setx PATH ""%PATH%;C:\Picidae_ADB"" && "
+    Private Const CleanupAdb As String = "rmdir /s /q C:\Picidae_ADB\platform-tools && del /s /q C:\Picidae_ADB\ADB_FASTBOOT.zip,C:\Picidae_ADB\Move_picidae.ps1"
+
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Shell("cmd.exe /ccd C:\ && mkdir Picidae_ADB && powershell Invoke-WebRequest -Uri http://dl.google.com/android/repository/platform-tools-latest-windows.zip -OutFile C:/Picidae_ADB/ADB_FASTBOOT.zip && powershell Expand-Archive C:\Picidae_ADB\ADB_FASTBOOT.zip -DestinationPath C:\Picidae_ADB && powershell.exe Invoke-WebRequest -Uri https://raw.githubusercontent.com/Far0/Android-Picidae/master/Android%20Picidae/fortheloveofgod.ps1 -OutFile C:\Picidae_ADB\Move_picidae.ps1 && powershell C:\Picidae_ADB\Move_picidae.ps1 && setx Picidae_ADB C:\Picidae_ADB\ && set PATH=%PATH%;C:\Picidae_ADB\ && cd C:\Picidae_ADB\ & rmdir /s /q platform-tools & del /s /q ADB_FASTBOOT.zip,Move_picidae.ps1", AppWinStyle.Hide, Wait:=True)
-        Shell("cmd.exe /csetx PATH ""%PATH%;C:\Picidae_ADB""", AppWinStyle.Hide)
-        MessageBox.Show(Text1, "", MessageBoxButtons.OK, MessageBoxIcon.Question, 0)
+        ' Interaction.Shell("cmd /csetx PATH ""%PATH%;C:\Picidae_ADB""", AppWinStyle.Hide)
+        Dim ADB As New ProcessStartInfo()
+        ADB.Verb = "runas"
+        ADB.FileName = "cmd.exe" ' 
+        ADB.Arguments = "/c " + "cd C:\ && " + "mkdir Picidae_ADB && " + Platform + UnzipAdb + MoveAdb + EnvPath + CleanupAdb
+        Try
+            Process.Start(ADB)
+        Catch
+            MsgBox("Y", 16, "")
+        End Try
+
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Process.Start("https://www.paypal.me/MauronofrioTool")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Interaction.Shell("cmd /ccd %APPDATA% && mkdir Picidae && adb shell getprop ro.product.model > %APPDATA%/Picidae/model", AppWinStyle.Hide, True, -1)
+        If (IO.File.Exists("model")) Then
+            MsgBox("moto")
+        Else
+            MsgBox("lul")
+        End If
+    End Sub
 End Class
